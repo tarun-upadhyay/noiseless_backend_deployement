8478378560953e9acdd23b2316a187bcf91a9412
tarun-upadhyay <tarunu88@gmail.com>
2022-12-16 01:20:06 +0530

    ad

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const express=require("express")
 const dotenv=require("dotenv").config()
 const connect=require("./config/db")
-const userRoute=require("./routes/userRouter")
+
 const {cors} = require("cors")
 const {productRouter }  = require("./routes/productRouter")
 const { pr_connection } = require("./config/pr_db")
 const app=express()
 app.use(cors())
 app.use(express.json())
-const PORT= process.env.PORT||5000
+const PORT= 8080
 app.get("/",(req,res)=>{
     res.send("homepage")
 })
 app.use("/products", productRouter)
-//routes middleware
-app.use("/api/users",userRoute)
+
 
 app.listen(PORT,async()=>{
     await pr_connection
-    await connect
+  
     console.log(`http://localhost:${PORT}`)
 })
